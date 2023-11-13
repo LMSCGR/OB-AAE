4814f311ea714475b8788cc1558644cfbeb38957
LMSCGR <109677034+LMSCGR@users.noreply.github.com>
2023-11-13 15:21:35 -0500

    Add files via upload

--- code/db_aae_tuning_example.py
+++ code/db_aae_tuning_example.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
+# In[1]:
 
 
 import numpy as np
@@ -16,7 +16,7 @@ import seaborn as sns
 import scanpy as sc
 
 
-# In[ ]:
+# In[2]:
 
 
 import random
@@ -25,14 +25,14 @@ random.seed(1234)
 
 # ## goolam
 
-# In[ ]:
+# In[3]:
 
 
-goolam= sc.read_h5ad("../pre_process_data/goolam.h5ad")
+goolam= sc.read_h5ad("./data/goolam.h5ad")
 goolam_sel=goolam
 
 
-# In[ ]:
+# In[4]:
 
 
 highly_genes=5000
@@ -46,7 +46,7 @@ sc.pp.highly_variable_genes(goolam_sel, min_mean=0.0125, max_mean=3, min_disp=0.
 goolam_sel = goolam_sel[:, goolam_sel.var['highly_variable']].copy()
 
 
-# In[ ]:
+# In[5]:
 
 
 #counts=sim_time.layers['counts']
@@ -57,26 +57,26 @@ cellinfo=pd.DataFrame(goolam_sel.obs['cell_type1'])
 geneinfo=pd.DataFrame(goolam_sel.var['feature_symbol'])
 
 
-# In[ ]:
+# In[6]:
 
 
 p_count=pd.DataFrame(counts)
 
 
-# In[ ]:
+# In[7]:
 
 
 p_count.index=cellinfo.index
 p_count.columns=geneinfo.index
 
 
-# In[ ]:
+# In[8]:
 
 
 p_count
 
 
-# In[ ]:
+# In[9]:
 
 
 m, n = p_count.shape
@@ -84,7 +84,7 @@ print('the num. of cell = {}'.format(m))
 print('the num. of genes = {}'.format(n))
 
 
-# In[ ]:
+# In[10]:
 
 
 adata = sc.AnnData(counts,obs=cellinfo,var=geneinfo)
@@ -96,7 +96,7 @@ adata = sc.AnnData(counts,obs=cellinfo,var=geneinfo)
 adata
 
 
-# In[ ]:
+# In[11]:
 
 
 from tensorflow.keras.models import Sequential, Model
@@ -109,7 +109,7 @@ import tensorflow as tf
 
 # ## Define Encoder
 
-# In[ ]:
+# In[12]:
 
 
 def build_encoder(n1,n2,n3,n4,activation):
@@ -132,7 +132,7 @@ def build_encoder(n1,n2,n3,n4,activation):
 
 # ## Define Decoder
 
-# In[ ]:
+# In[13]:
 
 
 def build_decoder(n1,n2,n3,n4,activation):
@@ -156,7 +156,7 @@ def build_decoder(n1,n2,n3,n4,activation):
 
 # ## Define Discriminator
 
-# In[ ]:
+# In[14]:
 
 
 def build_discriminator(n1,n2,n3,n4,activation):
@@ -176,7 +176,7 @@ def build_discriminator(n1,n2,n3,n4,activation):
         return Model(encoded_repr, validity)
 
 
-# In[ ]:
+# In[15]:
 
 
 import time
@@ -198,10 +198,145 @@ activation='relu'
 start_time = time.time()
 
 
+# In[16]:
+
+
+class AEHyperModel(kt.HyperModel):
+  
+  #def create_discriminator(n1,n2,n3,n4,activation,learning_rate):
+    
+  #  return discriminator
+
+  def build(self, hp):
+    hp_n2 = hp.Int('units_2', min_value=512, max_value=1024, step=32)
+    hp_n3 = hp.Int('units_3', min_value=32, max_value=512, step=32)
+    hp_n3 = hp.Int('units_3', min_value=512, max_value=1024, step=32)
+    hp_n4 = hp.Int('units_4', min_value=32, max_value=512, step=32)
+    #encoder=keras_3layer_encoder(n1,hp_n2,hp_n3,activation)
+    #decoder=keras_3layer_decoder(n1,hp_n2,hp_n3,activation)
+    #encoder=keras_4layer_encoder(n1,hp_n2,hp_n3,hp_n4,activation)
+    #decoder=keras_4layer_decoder(n1,hp_n2,hp_n3,hp_n4,activation)
+    # Build the encoder / decoder
+    encoder = build_encoder(n1,n2,n3,n4,activation)
+    decoder = build_decoder(n1,n2,n3,n4,activation) 
+    # Build and compile the discriminator
+    ##discriminator = build_discriminator(n1,n2,n3,n4,activation)
+    optimizer = RMSprop(learning_rate=0.00002)
+    ##discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    #autoencoder_input = Input(shape=(n1,))
+    #autoencoder = Model(autoencoder_input, decoder(encoder(autoencoder_input)))
+    discriminator = build_discriminator(n1,n2,n3,n4,activation)
+    discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    
+    autoencoder_input = Input(shape=(n1,))
+    reconstructed_input = Input(shape=(n1,))
+    encoded_repr = encoder(autoencoder_input)
+    reconstructed = decoder(encoded_repr)
+    
+    # For the adversarial_autoencoder model we will only train the generator
+    discriminator.trainable = False
+
+    # The discriminator determines validity of the encoding
+    validity = discriminator(encoded_repr)
+    
+
+    # The adversarial_autoencoder model  (stacked generator and discriminator)
+    adversarial_autoencoder = Model(autoencoder_input, [reconstructed, validity])
+    
+    ##hp_learning_rate = hp.Float('learning_rate', min_value=1e-4, max_value=0.01, sampling='LOG')
+    
+    #train_model =AAE(n1,n2,n3,n4,batch_size,activation,hp_learning_rate,50)
+    #train_model.train()
+    adversarial_autoencoder.compile(optimizer=optimizer,loss=['mse', 'binary_crossentropy'],loss_weights=[0.999, 0.001])
+    #autoencoder.compile(optimizer=RMSprop(learning_rate=hp_learning_rate), loss="binary_crossentropy", metrics=['accuracy'])
+    #autoencoder.compile(optimizer=Adagrad(learning_rate=hp_learning_rate), loss="binary_crossentropy", metrics=['accuracy'])
+    #autoencoder.compile(optimizer=Adam(learning_rate=hp_learning_rate), loss="binary_crossentropy", metrics=['accuracy'])
+    return adversarial_autoencoder
+    
+    
+  def fit(self, hp, adversarial_autoencoder, *args, **kwargs):
+    batch_size=hp.Int('batch_size', min_value = 16, max_value = 128, step = 8)
+    encoder = build_encoder(n1,n2,n3,n4,activation)
+    decoder = build_decoder(n1,n2,n3,n4,activation)
+    discriminator = build_discriminator(n1,n2,n3,n4,activation)
+    optimizer = RMSprop(learning_rate=0.00002)
+    discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+    # For the adversarial_autoencoder model we will only train the generator
+    discriminator.trainable = False
+    
+    valid = np.ones((batch_size, 1))
+    fake = np.zeros((batch_size, 1))
+    past = datetime.now()
+    for epoch in np.arange(1, n_epochs + 1):
+        for i in range(int(len(x) / batch_size)):
+
+            # ---------------------
+            #  Train Discriminator
+            # ---------------------
+
+            # Select a random batch of images
+            batch = x[i*batch_size:i*batch_size+batch_size]
+
+    
+            latent_fake = encoder.predict(batch)
+            latent_real = np.random.normal(size=(batch_size, latent_dim))
+
+            # Train the discriminator
+            d_loss_real = discriminator.train_on_batch(latent_real, valid)
+            d_loss_fake = discriminator.train_on_batch(latent_fake, fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+
+            # ---------------------
+            #  Train Generator
+            # ---------------------
+
+            # Train the generator
+            g_loss = adversarial_autoencoder.train_on_batch(batch, [batch, valid])
+       
+    return 100*d_loss[1]
+  #    return adversarial_autoencoder.fit(
+  #        *args,
+  #        batch_size=hp.Int('batch_size', min_value = 16, max_value = 128, step = 8),
+  #        **kwargs,
+      #)
+
+
+# In[28]:
+
+
+tuner = kt.Hyperband(AEHyperModel(),
+                    objective = 'val_accuracy',
+                    max_epochs = 10,
+                    executions_per_trial = 1,
+                    overwrite = True,
+                     directory = './cache_gloom',
+                     max_consecutive_failed_trials=5,
+                    factor = 3)
+
+
+# In[29]:
+
+
+stop_early = tf.keras.callbacks.EarlyStopping(monitor='val_accuracy', mode='max', baseline=100)
+tuner.search(x, x, epochs = 10, validation_split = 0.1, callbacks=[stop_early])
+
+
+# In[31]:
+
+
+best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
+
+print(f"""
+
+batch_size : {best_hps.get('batch_size')}
+""")
+
+
 # In[ ]:
 
 
 #optimizer = Adam(0.0002, 0.5)
+batch_size = best_hps.get('batch_size')
 optimizer = RMSprop(learning_rate=0.0002)
 # Build and compile the discriminator
 discriminator = build_discriminator(n1,n2,n3,n4,activation)
